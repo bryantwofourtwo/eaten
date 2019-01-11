2fba207d84e91bb723c3c88fc10d68b45e298018
bryantwofourtwo <bryantwofourtwo@gmail.com>
2019-01-11 15:39:52 -0700

    mountain game

---
+++ apps.js
@@ -0,0 +1,19 @@
+console.log('eaten, again');
+
+var eaten = hon;
+
+function () {
+    console.log('hon');
+}
+/* useless information */
+console.log('bastardo!');
+
+alert('Eat shit, cousin!');
+
+console.log('Why are you looking at this?')
+
+hon();
+
+home(yesterday);
+
+begat(shat);
